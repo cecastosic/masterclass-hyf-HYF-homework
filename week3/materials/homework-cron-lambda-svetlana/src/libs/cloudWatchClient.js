f350c2d9ecf1c0d27d10279bcce36dbcd31179e4
Svetlana Stosic Milosavljevic <cecastosic@gmail.com>
2021-09-26 08:13:10 +0200

    changed scheduler to show list of metrics

--- week3/materials/homework-cron-lambda-svetlana/src/libs/cloudWatchClient.js
+++
@@ -1,5 +0,0 @@
-import { CloudWatchClient } from "@aws-sdk/client-cloudwatch";
-// Set the AWS Region.
-const REGION = "us-east-1"
-// Create an Amazon CloudWatch service client object.
-export const cwClient = new CloudWatchClient({ region: REGION });
