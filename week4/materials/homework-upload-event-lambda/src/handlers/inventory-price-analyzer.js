653a4b8c6419637a5c184e84a9fa00656fb880ee
Svetlana Stosic Milosavljevic <cecastosic@gmail.com>
2021-10-03 12:39:19 +0200

    part one of homework week4

--- week4/materials/homework-upload-event-lambda/src/handlers/inventory-price-analyzer.js
+++ week4/materials/homework-upload-event-lambda/src/handlers/inventory-price-analyzer.js
@@ -1,25 +1,98 @@
-const AWS = require('aws-sdk');
+const AWS = require("aws-sdk");
 const s3 = new AWS.S3();
+require('dotenv').config();
+const fetch = (...args) =>
+  import("node-fetch").then(({ default: fetch }) => fetch(...args));
 
 exports.s3PriceAnalyzer = async (event) => {
-  const getObjectRequests = event.Records.map(record => {
+  const getObjectRequests = event.Records.map((record) => {
     const params = {
       Bucket: record.s3.bucket.name,
-      Key: record.s3.object.key
+      Key: record.s3.object.key,
     };
 
-    return s3.getObject(params).promise().then(data => {
-      // implement code here
-      let items = data.Body.toString().split("\n")
-      items.forEach(item => console.log(item))
+    return s3
+      .getObject(params)
+      .promise()
+      .then((data) => {
+        // implement code here
+        const items = data.Body.toString().split("\n");
+        const selectedItems = items
+          .map((item) => {
+            return {
+              category: item.split(",")[0],
+              price: Number(item.split(",")[2]),
+            };
+          })
+          .filter((item) => item.price < 10 && item.category === "fruits");
+        const prices = selectedItems.map((item) => item.price);
 
-    }).catch(err => {
-      console.error("Error calling S3 getObject:", err);
-      return Promise.reject(err);
-    })
+        console.log(JSON.stringify(selectedItems));
+        console.log({
+          maximumPrice: Math.max(...prices),
+          minimumPrice: Math.max(...prices),
+        });
+
+        return selectedItems;
+      })
+      .catch((err) => {
+        console.error("Error calling S3 getObject:", err);
+        return Promise.reject(err);
+      });
   });
 
-  return Promise.all(getObjectRequests).then(() => {
-    console.debug('Complete!');
+  return Promise.all(getObjectRequests).then(async () => {
+    console.debug("Complete!");
   });
-}
+};
+
+const fetchFunction = async () => {
+  const data = {
+    text: "Homework week 4",
+    blocks: [
+      {
+        type: "header",
+        text: {
+          type: "plain_text",
+          text: "Svetlana",
+        },
+      },
+      {
+        type: "section",
+        text: {
+          type: "plain_text",
+          text: "File: inventory-price-analyzer.js",
+        },
+      },
+      {
+        type: "section",
+        text: { type: "mrkdwn", text: `Number of selected items: ${2}` },
+      },
+      {
+        type: "section",
+        text: {
+          type: "mrkdwn",
+          text: "Maxiumum price: 2$",
+        },
+      },
+      {
+        type: "section",
+        text: { type: "mrkdwn", text: "Minimum price: 2$" },
+      },
+    ],
+  };
+
+  await fetch(
+    process.env.SLACK_URL,
+    {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(data),
+    }
+  )
+    .then((res) => res.json())
+    .then((data) => console.log(data))
+    .catch((err) => console.log(err));
+};
+
+fetchFunction();
